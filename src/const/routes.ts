f08b1d56214fb4cde208b7d75740d6ab8301b026
moisestech <m@moises.tech>
2022-08-21 09:42:43 -0400

    creations route

--- src/const/routes.ts
+++ src/const/routes.ts
@@ -1,10 +1,13 @@
 export const ROUTES = {
   TEST: {
-    HOME: '/test/home',
+    HOME: '/test/home'
   },
   WALLET: {
     CONNECT: '/wallet/connect',
     STORE: '/wallet/store',
-    USE: '/wallet/use',
+    USE: '/wallet/use'
+  },
+  CREATIONS: {
+    EDEN: '/creations/'
   }
-}
+};
