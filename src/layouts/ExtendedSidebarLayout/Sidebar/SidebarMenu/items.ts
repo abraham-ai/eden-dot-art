9ae9fb9d371ebaae27f83c4853a29abf3b4a6670
moisestech <m@moises.tech>
2022-08-23 12:50:27 -0400

    init list of routes

--- src/layouts/ExtendedSidebarLayout/Sidebar/SidebarMenu/items.ts
+++ src/layouts/ExtendedSidebarLayout/Sidebar/SidebarMenu/items.ts
@@ -1,6 +1,11 @@
 import type { ReactNode } from 'react';
 
+// ICONS
 import AccountTreeTwoToneIcon from '@mui/icons-material/AccountTreeTwoTone';
+import { MdHomeFilled } from 'react-icons/md';
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
+
+// ROUTES
 import { ROUTES } from '@/const/routes';
 
 export interface MenuItem {
@@ -19,6 +24,46 @@ export interface MenuItems {
 }
 
 const menuItems: MenuItems[] = [
+  {
+    heading: 'EDEN.DEV',
+    items: [
+      {
+        name: 'Home',
+        link: ROUTES.EDEN_DEV.HOME,
+        icon: MdHomeFilled
+      },
+      {
+        name: 'Pricing',
+        link: ROUTES.EDEN_DEV.PRICING,
+        icon: AttachMoneyIcon
+      }
+    ]
+  },
+  {
+    heading: 'EDEN.ART',
+    items: [
+      {
+        name: 'Home',
+        link: ROUTES.EDEN_DEV.HOME,
+        icon: MdHomeFilled
+      },
+      {
+        name: 'Pricing',
+        link: ROUTES.EDEN_DEV.PRICING,
+        icon: AttachMoneyIcon
+      },
+      {
+        name: 'Create',
+        link: ROUTES.EDEN_DEV.PRICING,
+        icon: AttachMoneyIcon
+      },
+      {
+        name: 'My Creations',
+        link: ROUTES.EDEN_DEV.PRICING,
+        icon: AttachMoneyIcon
+      }
+    ]
+  },
   {
     heading: 'Test',
     items: [
