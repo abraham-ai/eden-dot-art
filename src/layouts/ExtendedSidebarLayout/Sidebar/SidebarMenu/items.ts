6f86ed73094a465c1a8364b866dea9c1bc4b17e9
moisestech <m@moises.tech>
2022-08-24 21:50:49 -0400

    updated route tree

--- src/layouts/ExtendedSidebarLayout/Sidebar/SidebarMenu/items.ts
+++ src/layouts/ExtendedSidebarLayout/Sidebar/SidebarMenu/items.ts
@@ -1,10 +1,40 @@
 import type { ReactNode } from 'react';
 
-// ICONS
+// INFO ICONS
+import InfoIcon from '@mui/icons-material/Info';
 import AccountTreeTwoToneIcon from '@mui/icons-material/AccountTreeTwoTone';
 import { MdHomeFilled } from 'react-icons/md';
-import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
 
+// CREATE ICONS
+import CreateIcon from '@/components/CreateIcon';
+import VideoCallIcon from '@mui/icons-material/VideoCall';
+import CropLandscapeIcon from '@mui/icons-material/CropLandscape';
+import { TbRectangleVertical } from 'react-icons/tb';
+
+// SOCIAL 
+import ConnectWithoutContactIcon from '@mui/icons-material/ConnectWithoutContact';
+
+// SORT ICONS
+import WhatshotIcon from '@mui/icons-material/Whatshot';
+import BorderAllIcon from '@mui/icons-material/BorderAll';
+import PersonIcon from '@mui/icons-material/Person';
+import FlareIcon from '@mui/icons-material/Flare';
+import { RiMagicLine, RiMagicFill } from 'react-icons/ri';
+import { IoFlashOutline, IoFlashSharp } from 'react-icons/io5';
+import { AiFillFire, AiOutlineFire, AiFillApi } from 'react-icons/ai';
+
+// PRICNG ICONS
+import { RiCurrencyLine } from 'react-icons/ri';
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
+import CreditCardIcon from '@mui/icons-material/CreditCard';
+// import { BorderBottomOutlined } from '@ant-design/icons';
+import { TiArrowShuffle } from 'react-icons/ti';
+import {
+  HiSparkles,
+  HiOutlineSparkles,
+  HiCube,
+  HiOutlineCube
+} from 'react-icons/hi';
 // ROUTES
 import { ROUTES } from '@/const/routes';
 
@@ -28,14 +58,19 @@ const menuItems: MenuItems[] = [
     heading: 'EDEN.DEV',
     items: [
       {
-        name: 'Home',
+        name: 'About',
         link: ROUTES.EDEN_DEV.HOME,
-        icon: MdHomeFilled
+        icon: InfoIcon
       },
       {
-        name: 'Pricing',
+        name: 'Dev Pricing',
         link: ROUTES.EDEN_DEV.PRICING,
-        icon: AttachMoneyIcon
+        icon: RiCurrencyLine
+      },
+      {
+        name: 'API Documention',
+        link: ROUTES.EDEN_DEV.API_DOCUMENTATION,
+        icon: AiFillApi
       }
     ]
   },
@@ -44,24 +79,39 @@ const menuItems: MenuItems[] = [
     items: [
       {
         name: 'Home',
-        link: ROUTES.EDEN_DEV.HOME,
+        link: ROUTES.EDEN_ART.HOME,
         icon: MdHomeFilled
       },
       {
         name: 'Pricing',
-        link: ROUTES.EDEN_DEV.PRICING,
+        link: ROUTES.EDEN_ART.PRICING,
         icon: AttachMoneyIcon
       },
       {
+        name: 'Creations',
+        link: ROUTES.EDEN_ART.CREATIONS,
+        icon: ConnectWithoutContactIcon
+      },
+    ]
+  },
+  {
+    heading: 'Wallet',
+    items: [
+      {
         name: 'Create',
-        link: ROUTES.EDEN_DEV.PRICING,
-        icon: AttachMoneyIcon
+        link: ROUTES.WALLET.CREATE,
+        icon: CreateIcon
       },
       {
         name: 'My Creations',
-        link: ROUTES.EDEN_DEV.PRICING,
-        icon: AttachMoneyIcon
-      }
+        link: ROUTES.WALLET.MYCREATIONS,
+        icon: PersonIcon
+      },
+      {
+        name: 'Credits',
+        link: ROUTES.WALLET.CREDITS,
+        icon: CreditCardIcon
+      },
     ]
   },
   {
@@ -74,26 +124,6 @@ const menuItems: MenuItems[] = [
       }
     ]
   },
-  {
-    heading: 'Wallet',
-    items: [
-      {
-        name: 'Store',
-        link: ROUTES.WALLET.STORE,
-        icon: AccountTreeTwoToneIcon
-      },
-      {
-        name: 'Use',
-        link: ROUTES.WALLET.USE,
-        icon: AccountTreeTwoToneIcon
-      },
-      {
-        name: 'Creations',
-        link: '/creations',
-        icon: AccountTreeTwoToneIcon
-      }
-    ]
-  }
 ];
 
 export default menuItems;
