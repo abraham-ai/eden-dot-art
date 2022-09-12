6c661874451473c15463ffed9c945b80b2d9ef6a
moisestech <m@moises.tech>
2022-09-12 18:09:36 -0400

    eslint and gql data

--- pages/creations/index.tsx
+++ pages/creations/index.tsx
@@ -1,5 +1,11 @@
-// import { useState } from 'react'
+// import { useEffect } from 'react'
 import type { ReactElement } from 'react'
+
+// GQL
+import { useQuery } from '@apollo/client'
+import QueryResult from '@/components/QueryResult'
+
+// NEXT
 import Head from 'next/head'
 
 // NAV
@@ -14,7 +20,10 @@ import CreationCardMinimal from '@/components/CreationCardMinimal'
 import Masonry from '@mui/lab/Masonry'
 
 // CONSTS
-import { GET_CREATIONS } from '@/const/get-creations'
+// import { GET_CREATIONS } from '@/const/get-creations'
+
+// GQL Creations query to retreive all Creations //
+import { GET_CREATIONS as GQL_GET_CREATIONS } from '@/graphql/queries'
 
 const SelectStyles = styled('section')(
   () => `
@@ -25,11 +34,35 @@ const SelectStyles = styled('section')(
 )
 
 export default function CreationsPage() {
-  // label={
-  //   <>
-  //     <HiChip /> "Model"
-  //   </>
-  // }
+  const { loading, error, data, fetchMore } = useQuery(GQL_GET_CREATIONS, {
+    variables: {
+      offset: 0,
+      limit: 16,
+    },
+  })
+
+  // useEffect(() => {
+  //   console.log({ data })
+  // }, [data])
+
+  const onLoadMore = () =>
+    fetchMore({
+      variables: {
+        offset: 10,
+        limit: 10, //data.length
+      },
+      updateQuery: (prev, { fetchMoreResult }) => {
+        if (!fetchMoreResult) return prev
+        return {
+          creationsForHome: [
+            ...prev.creationsForHome,
+            ...fetchMoreResult.creationsForHome,
+          ],
+        }
+      },
+    })
+
+  // console.log(data)
 
   return (
     <>
@@ -53,6 +86,37 @@ export default function CreationsPage() {
 
           <Box sx={{ width: '100%', minHeight: 393, mt: 20 }}>
             <Masonry columns={4} spacing={2}>
+              <QueryResult error={error} loading={loading} data={data}>
+                {data?.creationsForHome?.map((creation, index) => (
+                  <CreationCardMinimal
+                    key={`${creation.id}_${index}`}
+                    creation={creation}
+                  />
+                ))}
+              </QueryResult>
+              <a href="#" onClick={onLoadMore}>
+                Load More
+              </a>
+            </Masonry>
+          </Box>
+        </Container>
+
+        {/* <Container maxWidth="xl">
+          {isConnected ? (
+          <>
+            <Typography variant={'h3'}>
+              You are connected with you wallet!
+            </Typography>
+            <Typography variant={'body1'}>{address}</Typography>
+          </>
+        ) : (
+          <Button variant={'contained'}>
+            <p>Please connect your Wallet</p>
+          </Button>
+        )}
+
+          <Box sx={{ width: '100%', minHeight: 393, mt: 20 }}>
+            <Masonry columns={4} spacing={2}>
               {GET_CREATIONS.map((creation, index) => {
                 const rand = Math.random()
                 if (rand > 0.5) {
@@ -64,7 +128,7 @@ export default function CreationsPage() {
               })}
             </Masonry>
           </Box>
-        </Container>
+        </Container> */}
       </SelectStyles>
       {/* <Footer /> */}
     </>
