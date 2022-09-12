74dd7993b9edb96e192d2d7169f3b6f2245d3670
moisestech <m@moises.tech>
2022-09-12 18:10:04 -0400

    create modal feature

--- src/components/CreateModal/index.tsx
+++ src/components/CreateModal/index.tsx
@@ -1,24 +1,25 @@
-import React, { useState, useEffect, useCallback } from 'react'
+import React, { useState } from 'react'
+// , useEffect, useCallback
 
 // STYLES
 // import './modalStyles.css'
 
 // MUI
 import {
-  alpha,
+  // alpha,
   Backdrop,
-  Button,
+  // Button,
   Box,
   FormControl,
   InputLabel,
-  lighten,
+  // lighten,
   MenuItem,
   Modal,
   Select,
   SelectChangeEvent,
-  styled,
+  // styled,
   TextField,
-  useTheme,
+  // useTheme,
 } from '@mui/material'
 
 // // REDUX
@@ -44,12 +45,12 @@ import {
 // } from '../../abraham'
 
 // HOOKS
-import useWindowDimensions from '@/hooks/useWindowDimensions'
+// import useWindowDimensions from '@/hooks/useWindowDimensions'
 
 // ICONS
-import { HiChip } from 'react-icons/hi'
-import { IoMdResize } from 'react-icons/io'
-import AddIcon from '@mui/icons-material/Add'
+// import { HiChip } from 'react-icons/hi'
+// import { IoMdResize } from 'react-icons/io'
+// import AddIcon from '@mui/icons-material/Add'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle'
 
 // LAYOUT ICONS
@@ -93,7 +94,7 @@ import { MdOutlineCropLandscape } from 'react-icons/md'
 // )
 
 const BoxModalStyle = {
-  position: 'absolute' as 'absolute',
+  position: 'absolute',
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
@@ -104,172 +105,172 @@ const BoxModalStyle = {
   p: 4,
 }
 
-const ModalStyles = styled('section')(
-  () => `
-  padding: 20px;
-  .ant-form-horizontal .ant-form-item-control {
-    max-width: unset;
-  }
-  .ant-progress-circle .ant-progress-inner {
-    margin-right: 18px;
-  }
-  ::placeholder {
-    color: #536471 !important;
-    opacity: 1;
-  }
-  
-  /* CREATE MODAL */
-  .create-modal .ant-modal-content {
-    width: 100%;
-    border-radius: 30px;
-    overflow: hidden;
-    padding: 40px 0 0 0;
-  }
-  #create-form {
-    width: 100%;
-    border: none;
-    padding: 0 0 0 20px;
-  }
-  .create-modal .form-wrapper {
-    display: flex;
-    flex-direction: row;
-    flex: 1;
-    width: 100%;
-  }
-  .create-modal .form-inner-wrapper {
-    display: flex;
-    flex-direction: column;
-    flex: 1;
-    width: 100%;
-  }
-  .create-modal button.ant-modal-close {
-    right: unset;
-    left: 0;
-  }
-  #create-form_prompt {
-    font-size: 1.5em;
-    border: none;
-    box-shadow: none;
-    width: 100%;
-    padding: 20px 0 0 0;
-  }
-  
-  /* SELECTOR */
-  #create-form
-    .ant-select-single.ant-select-show-arrow
-    .ant-select-selection-placeholder {
-    color: rgb(24, 144, 255);
-    font-weight: 600;
-  }
-  #create-form .ant-select-selector {
-    border-radius: 30px;
-  }
-  
-  /* FOOTER */
-  .create-modal .ant-modal-footer {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-  .create-modal.sign-in .ant-modal-footer {
-    justify-content: center;
-    align-items: center;
-  }
-  .create-modal .ant-modal-footer {
-    justify-content: flex-end;
-    flex: 1;
-  }
-  .create-modal .footer-wrapper {
-    display: flex;
-    align-items: center;
-    flex: 1;
-  }
-  .create-modal .footer-wrapper.sign-in {
-    justify-content: center;
-  }
-  .create-modal .footer-wrapper.create-button {
-    justify-content: flex-end;
-  }
-  
-  /* BUTTONS */
-  .create-modal button.sign-in-button,
-  .create-modal button.create-button {
-    width: 100px;
-    height: 50px;
-    display: flex;
-    /* border-radius: 50% !important; */
-    margin: 0;
-    padding: 0;
-    align-items: center;
-    justify-content: center;
-    border-width: 1px;
-    font-weight: 600;
-  }
-  .create-modal button.create-button span {
-    font-weight: 600;
-  }
-  
-  /* TEXT-AREA */
-  .create-modal .ant-form-item textarea.ant-input.create-text-area {
-    width: 100%;
-    border: none;
-    height: 150px;
-  }
-  .create-modal .ant-form-item-control {
-    width: 100%;
-    max-width: unset;
-  }
-  
-  /* SIGN-IN WRAPPER */
-  .sign-in-wrapper {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    width: 100%;
-  }
-  .sign-in-wrapper .header {
-    display: flex;
-    justify-content: center;
-  }
-  .sign-in-wrapper .header h3 {
-    font-size: 24px;
-    font-weight: 600;
-  }
-  .sign-in-wrapper .logo-wrapper {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
-  .sign-in-wrapper .logo-wrapper #logo {
-    width: unset;
-    height: 200px;
-  }
-  .sign-in-wrapper .message {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    padding: 30px 0;
-  }
-  .sign-in-wrapper .message p {
-    font-size: 18px;
-  }
-  .sign-in-wrapper .footer {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }  
-`,
-)
+// const ModalStyles = styled('section')(
+//   () => `
+//   padding: 20px;
+//   .ant-form-horizontal .ant-form-item-control {
+//     max-width: unset;
+//   }
+//   .ant-progress-circle .ant-progress-inner {
+//     margin-right: 18px;
+//   }
+//   ::placeholder {
+//     color: #536471 !important;
+//     opacity: 1;
+//   }
+
+//   /* CREATE MODAL */
+//   .create-modal .ant-modal-content {
+//     width: 100%;
+//     border-radius: 30px;
+//     overflow: hidden;
+//     padding: 40px 0 0 0;
+//   }
+//   #create-form {
+//     width: 100%;
+//     border: none;
+//     padding: 0 0 0 20px;
+//   }
+//   .create-modal .form-wrapper {
+//     display: flex;
+//     flex-direction: row;
+//     flex: 1;
+//     width: 100%;
+//   }
+//   .create-modal .form-inner-wrapper {
+//     display: flex;
+//     flex-direction: column;
+//     flex: 1;
+//     width: 100%;
+//   }
+//   .create-modal button.ant-modal-close {
+//     right: unset;
+//     left: 0;
+//   }
+//   #create-form_prompt {
+//     font-size: 1.5em;
+//     border: none;
+//     box-shadow: none;
+//     width: 100%;
+//     padding: 20px 0 0 0;
+//   }
+
+//   /* SELECTOR */
+//   #create-form
+//     .ant-select-single.ant-select-show-arrow
+//     .ant-select-selection-placeholder {
+//     color: rgb(24, 144, 255);
+//     font-weight: 600;
+//   }
+//   #create-form .ant-select-selector {
+//     border-radius: 30px;
+//   }
+
+//   /* FOOTER */
+//   .create-modal .ant-modal-footer {
+//     display: flex;
+//     justify-content: center;
+//     align-items: center;
+//   }
+//   .create-modal.sign-in .ant-modal-footer {
+//     justify-content: center;
+//     align-items: center;
+//   }
+//   .create-modal .ant-modal-footer {
+//     justify-content: flex-end;
+//     flex: 1;
+//   }
+//   .create-modal .footer-wrapper {
+//     display: flex;
+//     align-items: center;
+//     flex: 1;
+//   }
+//   .create-modal .footer-wrapper.sign-in {
+//     justify-content: center;
+//   }
+//   .create-modal .footer-wrapper.create-button {
+//     justify-content: flex-end;
+//   }
+
+//   /* BUTTONS */
+//   .create-modal button.sign-in-button,
+//   .create-modal button.create-button {
+//     width: 100px;
+//     height: 50px;
+//     display: flex;
+//     /* border-radius: 50% !important; */
+//     margin: 0;
+//     padding: 0;
+//     align-items: center;
+//     justify-content: center;
+//     border-width: 1px;
+//     font-weight: 600;
+//   }
+//   .create-modal button.create-button span {
+//     font-weight: 600;
+//   }
+
+//   /* TEXT-AREA */
+//   .create-modal .ant-form-item textarea.ant-input.create-text-area {
+//     width: 100%;
+//     border: none;
+//     height: 150px;
+//   }
+//   .create-modal .ant-form-item-control {
+//     width: 100%;
+//     max-width: unset;
+//   }
+
+//   /* SIGN-IN WRAPPER */
+//   .sign-in-wrapper {
+//     display: flex;
+//     flex-direction: column;
+//     justify-content: center;
+//     width: 100%;
+//   }
+//   .sign-in-wrapper .header {
+//     display: flex;
+//     justify-content: center;
+//   }
+//   .sign-in-wrapper .header h3 {
+//     font-size: 24px;
+//     font-weight: 600;
+//   }
+//   .sign-in-wrapper .logo-wrapper {
+//     display: flex;
+//     align-items: center;
+//     justify-content: center;
+//   }
+//   .sign-in-wrapper .logo-wrapper #logo {
+//     width: unset;
+//     height: 200px;
+//   }
+//   .sign-in-wrapper .message {
+//     display: flex;
+//     align-items: center;
+//     justify-content: center;
+//     padding: 30px 0;
+//   }
+//   .sign-in-wrapper .message p {
+//     font-size: 18px;
+//   }
+//   .sign-in-wrapper .footer {
+//     display: flex;
+//     align-items: center;
+//     justify-content: center;
+//   }
+// `,
+// )
 
 // const axios = require('axios')
 // const serverUrl = window?.appConfig?.ABRAHAM_GATEWAY
 
 export default function CreateModal({ isOpen = true, onClose }) {
-  const [confirmLoading, setConfirmLoading] = useState(false)
+  // const [confirmLoading, setConfirmLoading] = useState(false)
   const [prompt, setPrompt] = useState()
-  const [creationShape, setCreationShape] = useState('square')
-  const [creationHeight, setCreationHeight] = useState(1280)
-  const [creationWidth, setCreationWidth] = useState(1280)
+  // const [creationShape, setCreationShape] = useState('square')
+  // const [creationHeight, setCreationHeight] = useState(1280)
+  // const [creationWidth, setCreationWidth] = useState(1280)
   const [maxCharPercent, setMaxCharPercent] = useState(0)
 
   const [generator, setGenerator] = useState('stable-diffusion')
@@ -317,10 +318,10 @@ export default function CreateModal({ isOpen = true, onClose }) {
   // }
 
   const onChange = e => {
-    console.log('Change:', e.target.value)
+    // console.log('Change:', e.target.value)
     setPrompt(e.target.value)
     setMaxCharPercent(Math.ceil((e.target.value.length / 200) * 100))
-    console.log(maxCharPercent)
+    // console.log(maxCharPercent)
   }
 
   // const onReset = () => {
@@ -636,6 +637,7 @@ export default function CreateModal({ isOpen = true, onClose }) {
                 onChange={onChange}
               />
             </FormControl>
+            {maxCharPercent}
           </Box>
         </Box>
       </Box>
