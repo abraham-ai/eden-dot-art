2d1ca604a40aef41ca9ffe708bf8e15d791dcd33
moisestech <m@moises.tech>
2022-10-01 22:29:00 -0400

    theme options

--- src/const/env.ts
+++ src/const/env.ts
@@ -7,6 +7,31 @@ export const appConfig = {
   ABRAHAM_MINIO: 'https://minio.aws.abraham.fun/creations-stg/',
   ABRAHAM_GRAPH:
     'https://graph.dev.aws.abraham.fun/subgraphs/name/scaffold-eth/your-contract',
-  AUTH0_DOMAIN: 'moisestech.us.auth0.com',
-  AUTH0_CLIENT_ID: '4qfEwH84D1shBOheIDgcMH8Uxl99kvTK'
-};
+}
+
+// export default function appEnvs() {
+//   let appConfig = {}
+
+//   if (typeof window !== 'undefined') {
+//     window.appConfig = {
+//       ABRAHAM_SELF: 'https://app.stg.aws.abraham.fun',
+//       ABRAHAM_CHAIN: 'https://chain.stg.aws.abraham.fun',
+//       ABRAHAM_GATEWAY: 'https://gateway.stg.aws.abraham.fun',
+//       ABRAHAM_IPFS: 'https://ipfs-gateway.stg.aws.abraham.fun/ipfs',
+//       ABRAHAM_MINIO: 'https://minio.aws.abraham.fun/creations-stg/',
+//       ABRAHAM_GRAPH:
+//         'https://graph.dev.aws.abraham.fun/subgraphs/name/scaffold-eth/your-contract',
+//     }
+//   } else {
+//     appConfig = {
+//       ABRAHAM_SELF: '',
+//       ABRAHAM_CHAIN: '',
+//       ABRAHAM_GATEWAY: '',
+//       ABRAHAM_IPFS: '',
+//       ABRAHAM_MINIO: '',
+//       ABRAHAM_GRAPH: '',
+//     }
+//   }
+
+//   return appConfig
+// }
