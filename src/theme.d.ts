f151d1d47bef6e9372b9ae7b10302874569caac4
moisestech <m@moises.tech>
2022-10-01 22:03:32 -0400

    init theme d ts

---
+++ src/theme.d.ts
@@ -0,0 +1,27 @@
+import { ThemeOptions } from '@mui/material/styles'
+import React from 'react'
+
+declare module '@mui/material/styles' {
+  interface Theme {
+    bg: {
+      main: string
+    }
+  }
+  interface ThemeOptions {
+    bg: {
+      main: React.CSSProperties['color']
+    }
+  }
+
+  interface Palette {
+    neutral?: PaletteColor
+  }
+
+  interface PaletterOptions {
+    neutral?: PalleteColorOptions
+  }
+
+  interface SimplePaletteColorOptions {
+    darker?: string
+  }
+}
