dc9489d6b4ef9fd8e6f2f293575e286dd948c2a4
moisestech <m@moises.tech>
2023-01-30 22:43:45 -0500

    component to display in views

---
+++ src/components/Create/CreateButton/CreateButton.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+
+// REDUX
+import { setModalVisible } from '@/redux/slices/modalSlice'
+import { useAppSelector, useAppDispatch } from '@/hooks/hooks' // useSignMessage
+
+
+// ANTD
+import { Button, Typography } from 'antd'
+const { Text } = Typography
+
+// STYLES
+import styled from 'styled-components'
+
+// ICONS
+import AddIcon from '@mui/icons-material/Add'
+
+const CreateButtonStyles = styled.div`
+	#create-button {
+		display: flex;
+		align-items: center;
+		height: 45px;
+		margin-left: 15px;
+		padding: 10px 20px;
+		color: white;
+		background: #8C7CF0;
+		border-radius: 30px;
+	}
+`
+
+export default function CreateButton() {
+
+		// inti redux fetchers/getters
+		const dispatch = useAppDispatch();
+
+    const handleCreateOpen = () => {
+			// console.log('HANDLE-CREATE OPEN!')
+			dispatch(setModalVisible(true))
+		}
+
+    return (
+        <CreateButtonStyles id='create-button-wrapper'>
+            <Button
+                id='create-button'
+                onClick={handleCreateOpen}
+                size='middle'
+                icon={<AddIcon className='create-icon' fontSize={'medium'} />}
+              >
+                <Text style={{ fontWeight: 'bold', color: 'white', fontSize: '1rem', marginLeft: 10 }}>
+                  Create
+                </Text>
+            </Button>
+        </CreateButtonStyles>        
+    )
+}
