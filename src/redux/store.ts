101b57c295ebe31f61ff17b5da89272610ce45d2
moisestech <m@moises.tech>
2023-02-06 14:12:26 -0600

    implementing next-redux-wrapper

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,4 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { createWrapper } from 'next-redux-wrapper'
 
 // SLICES
 import {
@@ -15,24 +16,26 @@ import {
   modalReducer,
 } from './slices/'
 
-export const store = configureStore({
-  reducer: {
-    accountButton: accountButtonReducer,
-    counter: counterReducer,
-    allTokens: allTokensReducer,
-    filter: filterReducer,
-    sort: sortReducer,
-    creations: creationsReducer,
-    address: addressReducer,
-    auth: authReducer,
-    token: tokenReducer,
-    snackbar: snackbarReducer,
-    modal: modalReducer,
-  },
-})
+const makeStore = () => 
+  configureStore({
+    reducer: {
+      accountButton: accountButtonReducer,
+      counter: counterReducer,
+      allTokens: allTokensReducer,
+      filter: filterReducer,
+      sort: sortReducer,
+      creations: creationsReducer,
+      address: addressReducer,
+      auth: authReducer,
+      token: tokenReducer,
+      snackbar: snackbarReducer,
+      modal: modalReducer,
+    },
+    devTools: true
+  })
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+// export type RootState = ReturnType<typeof store.getState>
+// export type AppDispatch = typeof store.dispatch
 
-export default store
+export const wrapper = createWrapper(makeStore)
