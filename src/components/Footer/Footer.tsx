fed13518470b47230b68e400f4383417f01d24d1
moisestech <m@moises.tech>
2023-02-09 13:34:14 -0600

    tsc-noemit cntd

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -9,100 +9,78 @@ import Link from 'next/link'
 import styled from 'styled-components'
 
 const FooterWrapper = styled.section`
-    .footer-info-section {
-      padding: 10px;
-      display: flex;
-      flex-direction: column;
-      align-items: flex-start;
-    }
-    .footer-link {
-      padding: 8px 0;
-      color: #0c163b;
-    }
-`,
-)
+  .footer-info-section {
+    padding: 10px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+  }
+  .footer-link {
+    padding: 8px 0;
+    color: #0c163b;
+  }
+`
 
 function Footer() {
   return (
     <FooterWrapper className="footer-wrapper">
       <div
-        style={{ paddingBottom: 40, display: 'flex', alignItems: 'center', textAlign: 'center', justifyContent: 'space-between' }} >
+        style={{
+          paddingBottom: 40,
+          display: 'flex',
+          alignItems: 'center',
+          textAlign: 'center',
+          justifyContent: 'space-between',
+        }}
+      >
         <div className="footer-info-section">
           <AppLogo logo="eden" />
-          <Text style={{ color: '#0c163b' }}>
-            Learn
-          </Text>
+          <Text style={{ color: '#0c163b' }}>Learn</Text>
 
           <Link href="/create">
-            <Text className="footer-link">
-              Create
-            </Text>
+            <Text className="footer-link">Create</Text>
           </Link>
           <Link href="/collect">
-            <Text className="footer-link">
-              Collect
-            </Text>
+            <Text className="footer-link">Collect</Text>
           </Link>
           <Link href="/docs">
-            <Text className="footer-link">
-              Documentation
-            </Text>
+            <Text className="footer-link">Documentation</Text>
           </Link>
         </div>
 
         <div className="footer-info-section">
-          <Text className="footer-link">
-            Company
-          </Text>
+          <Text className="footer-link">Company</Text>
 
           <Link href="/dev-about">
-            <Text className="footer-link">
-              About
-            </Text>
+            <Text className="footer-link">About</Text>
           </Link>
           <Link href="/help">
-            <Text className="footer-link">
-              Help Center
-            </Text>
+            <Text className="footer-link">Help Center</Text>
           </Link>
           <Link href="/subscribe">
-            <Text className="footer-link">
-              Subscribe
-            </Text>
+            <Text className="footer-link">Subscribe</Text>
           </Link>
         </div>
 
         <div className="footer-info-section">
-          <Text className="footer-link">
-            Connect
-          </Text>
+          <Text className="footer-link">Connect</Text>
           <Link href="twitter.com">
-            <Text className="footer-link">
-              Twitter
-            </Text>
+            <Text className="footer-link">Twitter</Text>
           </Link>
           <Link href="instagram.com">
-            <Text className="footer-link">
-              Instagram
-            </Text>
+            <Text className="footer-link">Instagram</Text>
           </Link>
           <Link href="discord.com">
-            <Text className="footer-link">
-              Discord
-            </Text>
+            <Text className="footer-link">Discord</Text>
           </Link>
         </div>
 
         <div className="footer-info-section">
-          <Text className="footer-link">
-            Privacy Policy
-          </Text>
+          <Text className="footer-link">Privacy Policy</Text>
         </div>
 
         <div className="footer-info-section">
-          <Text className="footer-link">
-            Terms of Service
-          </Text>
+          <Text className="footer-link">Terms of Service</Text>
         </div>
       </div>
     </FooterWrapper>
