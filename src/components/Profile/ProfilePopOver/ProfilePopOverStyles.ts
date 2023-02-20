868cc5332364586b7095730307decab245269ed3
moisestech <m@moises.tech>
2023-02-20 08:42:53 -0500

    add styles file outside component

---
+++ src/components/Profile/ProfilePopOver/ProfilePopOverStyles.ts
@@ -0,0 +1,10 @@
+// STYLES
+import styled from 'styled-components'
+
+const ProfilePopOverStyles = styled.section`
+  .popper-wrapper {
+    padding: 20px;
+  }
+`
+
+export { ProfilePopOverStyles }
