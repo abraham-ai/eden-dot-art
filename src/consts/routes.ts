df8f22035d1cecf828fad431d9000b932b8bebc2
moisestech <m@moises.tech>
2023-02-20 08:43:27 -0500

    new renamed consts to import static vars

---
+++ src/consts/routes.ts
@@ -0,0 +1,17 @@
+export const ROUTES = {
+  EDEN_DEV: {
+    DEV_GARDEN: '/garden',
+  },
+  EDEN_ART: {
+    HOME: '/',
+    CREATIONS: '/creations',
+    WATCH: '/watch',
+  },
+  CREATE: {
+    MAIN: '/create',
+    CREATORS: '/creators',
+  },
+  WALLET: {
+    CREATE: '/create',
+  },
+}
