634fc28f1f50f659a6ae38021a01048dcae82aa7
moisestech <m@moises.tech>
2023-02-20 08:37:41 -0500

    refactor styles and logic for all creation components

--- pages/creation/index.tsx
+++ pages/creation/index.tsx
@@ -1,12 +1,13 @@
-// useEffect
-import { useState } from 'react' // useMemo,
+import { useState } from 'react'
+
+// TYPES
 import type { ReactElement } from 'react'
+import Creation from '@/interfaces/Creation'
 
 // NEXT
 import Head from 'next/head'
 
 // NAV
-// import Footer from '@/components/Footer'
 import BaseLayout from 'src/layouts/BaseLayout'
 
 // LIBS
@@ -16,64 +17,16 @@ import Masonry from 'react-masonry-css'
 import CreationCard from '@/components/Creation/CreationCard/CreationCard'
 import Loader from '@/components/Loader/Loader'
 
-// HOOKS
-// import useWindowDimensions from '@/hooks/useWindowDimensions'
-
 // CONSTS
-const PAGE_LENGTH = 10
-// import { DEVICE_WIDTH } from '@/const/device-width'
 const masonryOptions = { transitionDuration: 0 }
-const imagesLoadedOptions = { background: '.my-bg-image-el' }
+const imagesLoadedOptions = { background: '.cr-grid-masonry-bg-image-el' }
+import { breakpointColumnsObj } from '@/consts/breakpointColumns'
+
+// STYLES
+import { CreationStyles } from './CreationStyles'
 
 export default function CreationsPage() {
-  const [creations, setCreations] = useState<object[]>([])
-  const [breakpointCols] = useState(3) // setBreakpointCols
-
-  // CUSTOM HOOKS
-  // const { width } = useWindowDimensions()
-
-  // useEffect(() => {
-  //   console.log({ data })
-  // }, [data])
-
-  // console.log(data)
-
-  // const getBreakpointCols = useMemo(() => {
-  //   const { MOBILE, TABLET, DESKTOP, DESKTOP_XL, DESKTOP_XXL } = DEVICE_WIDTH
-  //   // console.log('USE-MEMO BREAKPOINTS!!!!')
-  //   // console.log(width)
-  //   // console.log(width <= 960)
-
-  //   if (width <= MOBILE) {
-  //     // console.log('USE-MEMO BREAKPOINTS MOBILE!!!!')
-  //     setBreakpointCols(1)
-  //     return 1
-  //   } else if (width >= MOBILE && width <= TABLET) {
-  //     // console.log('USE-MEMO BREAKPOINTS TABLET!!!!')
-  //     setBreakpointCols(2)
-  //     return 2
-  //   } else if (width >= TABLET && width <= DESKTOP) {
-  //     // console.log('USE-MEMO BREAKPOINTS TABLET!!!!')
-  //     setBreakpointCols(3)
-  //     return 2
-  //   } else if (width >= DESKTOP && width < DESKTOP_XL) {
-  //     // console.log('USE-MEMO BREAKPOINTS DESKTOP!!!!')
-  //     setBreakpointCols(4)
-  //     return 4
-  //   } else if (width >= DESKTOP_XL && width < DESKTOP_XXL) {
-  //     setBreakpointCols(4)
-  //     return 6
-  //   } else if (width >= DESKTOP_XXL) {
-  //     setBreakpointCols(4)
-  //     return 7
-  //   } else {
-  //     // console.log('USE MEMO DEFAULT!!!')
-  //   }
-  // }, [width])
-
-  // console.log(breakpointCols)
-  // console.log(getBreakpointCols)
-  // console.log({ data })
+  const [creations] = useState<Creation[]>([])
 
   return (
     <>
@@ -85,20 +38,21 @@ export default function CreationsPage() {
         {creations.length < 1 ? (
           <Loader />
         ) : (
-          <div style={{ width: '100%', minHeight: 393, mt: 20 }}>
+          <CreationStyles id="creation-masonry-wrapper">
             <Masonry
-              className={'my-gallery-class'} // default ''
-              elementType={'ul'} // default 'div'
+              breakpointCols={breakpointColumnsObj}
+              className={'cr-grid-masonry'}
+              columnClassName="cr-grid-masonry_column"
               options={masonryOptions} // default {}
               disableImagesLoaded={false} // default false
               updateOnEachImageLoad={false} // default false and works only if disableImagesLoaded is false
               imagesLoadedOptions={imagesLoadedOptions} // default {}
             >
-              {creations.map((creation, index) => (
-                <CreationCard key={index} creation={creation} />
+              {creations.map((creation, i: number) => (
+                <CreationCard creation={creation} key={i} />
               ))}
             </Masonry>
-          </div>
+          </CreationStyles>
         )}
       </section>
     </>
