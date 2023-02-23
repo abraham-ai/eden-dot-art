e0a4dcb63919c3b5f8f27a7efae4093c748daf3f
moisestech <m@moises.tech>
2023-02-22 22:52:39 -0500

    start debugging middleware fire /creation /garden redirect

--- pages/middleware.ts
+++ pages/middleware.ts
@@ -1,23 +1,12 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-// IRON-SESSION
-import { getIronSession } from 'iron-session/edge'
-import { sessionOptions } from '@/util/withSession'
-
 export const middleware = async (req: NextRequest) => {
-  const res = NextResponse.next()
-  const session = await getIronSession(req, res, sessionOptions)
-
-  const { userId } = session.get('user')
-  const { token } = session.get('token')
-
-  console.log('from middleware', { userId, token })
-
+  // console.log('from middleware', { userId, token })
   // demo:
-  if (userId !== '0x49fbd13846F2428c148A4c165a22b4fFA54263a4') {
-    return new NextResponse(null, { status: 403 }) // unauthorized to see pages inside admin/
-  }
+  // if (userId !== '0x49fbd13846F2428c148A4c165a22b4fFA54263a4') {
+  //   return new NextResponse(null, { status: 403 }) // unauthorized to see pages inside admin/
+  // }
 
   const url = req.nextUrl.clone()
 
