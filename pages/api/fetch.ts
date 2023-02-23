2841aa2688a7b8a27e0342ab8866d95daf813578
moisestech <m@moises.tech>
2023-02-22 21:03:52 -0500

    debug session data to remoce the duplicate token.token access

--- pages/api/fetch.ts
+++ pages/api/fetch.ts
@@ -4,35 +4,37 @@ import { eden } from '../../src/util/eden'
 
 interface ApiRequest extends NextApiRequest {
   body: {
-    taskId: string;
-  };
+    taskId: string
+  }
 }
 
 const handler = async (req: ApiRequest, res: NextApiResponse) => {
   // const { taskId } = req.body;
-  const authToken = req.session.token;
+  const authToken = req.session.token
+
+  // console.log('fetch.js — req.session', req.session)
+  // console.log('fetch.js — authToken', authToken)
 
   if (!authToken) {
-    return res.status(401).json({ error: "Not authenticated" });
+    return res.status(401).json({ error: 'Not authenticated' })
   }
 
   try {
-    eden.setAuthToken(authToken.token);
-    const result = await eden.getTasks(["running", "pending"]);
+    eden.setAuthToken(authToken)
+    const result = await eden.getTasks(['running', 'pending'])
     if (result.error) {
       // console.log("result.error found")
       // console.log(result.error)
-      return res.status(500).json({ error: result.error });
-    } 
-    
-    return res.status(200).json(result);
-    
+      return res.status(500).json({ error: result.error })
+    }
+
+    return res.status(200).json(result)
   } catch (error: any) {
     // console.log(error)
     // console.log("FETCH CAUGHT AN ERORR")
     // console.log(error)
-    return res.status(500).json({ error: error.response.data });
+    return res.status(500).json({ error: error.response.data })
   }
-};
+}
 
-export default withSessionRoute(handler);
+export default withSessionRoute(handler)
