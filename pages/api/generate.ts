6ac59c795d2e09efac8cb401cafb4cfd1840a136
moisestech <m@moises.tech>
2023-02-22 21:05:19 -0500

    debug session data to remoce the duplicate token.token access

--- pages/api/generate.ts
+++ pages/api/generate.ts
@@ -2,32 +2,33 @@ import { NextApiRequest, NextApiResponse } from 'next/types'
 import { withSessionRoute } from '@/util/withSession'
 import { eden } from '../../src/util/eden'
 
+// TYPES
+import Config from '@/interfaces/Config'
 
 interface ApiRequest extends NextApiRequest {
   body: {
-    generatorName: string;
-    config: any;
-  };
+    generatorName: string
+    config: Config
+  }
 }
 
 const handler = async (req: ApiRequest, res: NextApiResponse) => {
-  const { config, generatorName } = req.body;
-  const authToken = req.session.token;
+  const { config, generatorName } = req.body
+  const authToken = req.session.token
 
   if (!authToken) {
     return res.status(401).json({ error: 'Not authenticated' })
   }
 
   try {
-    eden.setAuthToken(authToken.token)
+    eden.setAuthToken(authToken)
     //const userId = req.session.userId;
-    const result = await eden.startTask(generatorName, config);
+    const result = await eden.startTask(generatorName, config)
     if (result.error) {
-      return res.status(500).json({ error: result.error });
-    } 
-    else {
+      return res.status(500).json({ error: result.error })
+    } else {
       // console.log(`Starting task ${result.taskId}...`)
-      return res.status(200).json({ taskId: result.taskId });
+      return res.status(200).json({ taskId: result.taskId })
     }
   } catch (error: any) {
     if (error.response.data == 'jwt expired') {
