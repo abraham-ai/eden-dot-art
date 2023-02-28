6cb7b597522c6f7e1a44ffe700145b005d45eab9
moisestech <m@moises.tech>
2023-02-28 08:44:59 -0500

    ApiKeys interface

--- src/hooks/useApiKeys.ts
+++ src/hooks/useApiKeys.ts
@@ -1,8 +1,15 @@
 import useSWR from 'swr'
 import { fetcher } from '@/util/fetcher'
 
+interface ApiKeysResponse {
+  apiKeys: string[]
+}
+
 export const useApiKeys = () => {
-  const { data, error, isLoading, mutate } = useSWR('/api/user/keys', fetcher)
+  const { data, error, isLoading, mutate } = useSWR<ApiKeysResponse>(
+    '/api/user/keys',
+    fetcher,
+  )
 
   return {
     apiKeys: data?.apiKeys,
